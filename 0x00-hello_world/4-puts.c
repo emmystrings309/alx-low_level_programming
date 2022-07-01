210347627a2656f7e1342790f33d0436c719802a
Alabi Okikijesu <emmystrings001@gmail.com>
2022-06-30 21:14:41 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
