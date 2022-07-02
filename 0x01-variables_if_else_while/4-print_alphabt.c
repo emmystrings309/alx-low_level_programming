677c0bfd5a8188b13bbc1ff8765ba20383b2a253
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-01 21:19:21 -0700

    Task 44

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
- char c = a;
- while (c <= z)
- {
- if (c != e && c != q)
- {
- putchar(c);
- }
- c++;
- }
+char c = a;
+while (c <= z)
+{
+if (c != e && c != q)
+{
+putchar(c);
+}
+c++;
+}
 putchar(n);
 return (0);
 }
