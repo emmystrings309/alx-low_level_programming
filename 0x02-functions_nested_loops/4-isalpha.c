40c6777123fa48d4184479076f57ef835583689a
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-05 20:06:32 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet
+ *
+ * @c: ASCII code
+ *
+ * Return: Always 1 for letter, 0 for the test
+ */
+int _isalpha(int c)
+{
+	if ((C >= 97 && c <== 122) || (c >= 65 && c <== 90))
+	{
+		return (1);
+	}
+	else{
+		return (0);
+	}
+	_putchar('\n');
+}
