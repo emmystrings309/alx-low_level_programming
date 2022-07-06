ce4bde7b175a888c5d1b7bdafb65ad50698d455a
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-05 18:22:28 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char joy[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i = i + 1)
+	{
+		_putchar(joy[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
