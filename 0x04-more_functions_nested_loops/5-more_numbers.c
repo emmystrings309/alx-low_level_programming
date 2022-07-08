183f24080e43fb4061f166377bfc6061b85fea1e
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-07 18:43:15 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 0; k < 15; k++)
+		{
+			if (k >= 10)
+				_putchar((k / 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
