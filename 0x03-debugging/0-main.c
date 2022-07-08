a7ea0b7fd592ce5032949d0f4a28732317532f7f
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-07 21:02:56 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function thst prints if integer is positively
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i= 0;
+	positive_or_negative(i);
+
+	return (0);
+}
