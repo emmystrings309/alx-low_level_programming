d37df1419425a183a16dae36b04216ccf862fe72
Alabi Okikijesu <emmystrings001@gmail.com>
2022-07-07 18:51:24 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print lines
+ * *@n: line
+ * Return: void
+ */
+void prunt_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; 1++);
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
